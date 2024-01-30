d24d4abc2f931952375c3095b45d5df6c2b6a8c4
Shanilka Gurusinghe <128635738+SMadu99@users.noreply.github.com>
2024-01-30 23:40:11 +0530

    remove VogDoctor folder personal.css file form css parts

--- src/Pages/Family/FamilyDashboard.jsx
+++ src/Pages/Family/FamilyDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box, IconButton } from "@mui/material";
-import Header from "../../C/Header";
+import Header from "../../Components/Header";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
