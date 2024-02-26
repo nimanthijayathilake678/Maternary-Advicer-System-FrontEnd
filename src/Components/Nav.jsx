2c75a96a88868c02b342e8851616225f4c63e646
Binishi-0822 <binishipiyumika3@gmail.com>
2024-02-26 22:07:39 +0530

    Change the Navigation

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -6,9 +6,10 @@ import { Fragment } from "react";
 import useAuth from "../Hooks/useAuth";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
+import Logo from "../Assest/logo.png";
 
 const Nav = () => {
-  const { auth } = useAuth();
+  const { user } = useAuth();
   const location = useLocation();
 
   const navItems = [
@@ -61,6 +62,8 @@ const Nav = () => {
     };
   }
 
+  console.log("Auth : " + user);
+
   return (
     <Disclosure
       as="header"
@@ -69,7 +72,7 @@ const Nav = () => {
       {({ open }) => (
         <>
           <div className="w-full px-2 mx-auto sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16 ">
+            <div className="relative flex items-center justify-between h-20"> {/* Change h-16 to h-20 or desired height */}
               <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                 <Disclosure.Button className="relative inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-700 hover:text-white">
                   <span className="absolute-inset-0.5" />
@@ -112,14 +115,14 @@ const Nav = () => {
                 </Disclosure.Button>
               </div>
 
-              <div className="flex items-center justify-between w-full">
+              <div className="flex items-center justify-between w-full ">
                 <div className="flex items-center">
                   <div className="flex items-center-shrink-0">
                     <a href="/">
                       <img
-                        className="w-auto h-10"
-                        src="logo.jpeg"
-                        alt="Maternity Advisor Logo"
+                        className="w-36 h-auto"
+                        src= {Logo}
+                        alt="Maternity Adviser Logo"
                       />
                     </a>
                   </div>
@@ -144,21 +147,21 @@ const Nav = () => {
                     </div>
                   </div>
                 </div>
-                {!auth ? (
-                  <Button to="./login" component={Link} variant="contained">
+                {!user ? (
+                  <Button to="./login" component={Link} variant="contained" sx={{backgroundColor:'#00A9BB'}}>
                     Login
                   </Button>
                 ) : (
                   <Menu as="div" className="ml-3 ">
                     <div className="items-center hidden sm:flex ">
                       <span className="mr-3 text-[#2A777C] font-semibold">
-                        {auth?.name}
+                        {user?.Name}
                       </span>
                       <Menu.Button className="relative text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus-ring-offset-gray-800">
                         <span className="absolute-inset-1.5" />
                         <span className="sr-only">Open user menu</span>
 
-                        <Avatar {...stringAvatar(auth?.name)} />
+                        <Avatar {...stringAvatar(user?.fullName)} />
                       </Menu.Button>
                     </div>
 
@@ -175,10 +178,10 @@ const Nav = () => {
                         <Menu.Item>
                           {({ active }) => (
                             <a
-                              href={"/" + auth.authRole + "/profile"}
+                              href={"/" + user.position + "/profile"}
                               className={classNames(
                                 active ? "bg-[#00A9BB]" : "",
-                                "block px-4 py-2 text-sm text-white-700"
+                                "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
                               My Profile
@@ -189,10 +192,10 @@ const Nav = () => {
                         <Menu.Item>
                           {({ active }) => (
                             <a
-                              href={"/" + auth.authRole}
+                              href={"/" + user.position}
                               className={classNames(
                                 active ? "bg-[#00A9BB]" : "",
-                                "block px-4 py-2 text-sm text-white-700"
+                                "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
                               Dashboard
@@ -206,7 +209,7 @@ const Nav = () => {
                               href="#"
                               className={classNames(
                                 active ? "bg-[#00A9BB]" : "",
-                                "block px-4 py-2 text-sm text-white-700"
+                                "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
                               Setting
@@ -237,16 +240,16 @@ const Nav = () => {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {!auth ? (
+              {!user ? (
                 <Button to="./login" component={Link} variant="contained">
                   Login
                 </Button>
               ) : (
                 <div className="flex justify-center gap-4 items-center">
                   <span className="mr-3 text-[#2A777C] font-semibold">
-                    {auth.name}
+                    {user.fullname}
                   </span>
-                  <Avatar {...stringAvatar(auth.name)} />
+                  <Avatar {...stringAvatar(user.fullname)} />
                 </div>
               )}
               <div></div>
