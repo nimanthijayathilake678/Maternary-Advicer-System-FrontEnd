ee3914a5a24c00b035c2de48d2a337b31889b12c
Shanilka Gurusinghe <128635738+SMadu99@users.noreply.github.com>
2024-04-25 01:16:13 +0530

    Update files

--- src/Pages/MOHAdmin/View.jsx
+++ src/Pages/MOHAdmin/View.jsx
@@ -103,6 +103,8 @@ const View = () => {
 export default View;
 */
 
+
+/*
 import "../../css/MOHAdmin/View.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
@@ -218,3 +220,93 @@ const View = () => {
 };
 
 export default View;
+*/
+
+
+import React, { useState, useEffect } from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import SideBar from "../../Components/SideBar";
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import DisplaySidebar from "../../Components/DisplaySidebar";
+import {
+  getClinicDates,
+} from "../../Services/ClinicSchedule";
+
+
+const VISIBLE_FIELDS = [
+    
+        "regNum",
+        "fullName",
+        "firstName",
+        "lastName",
+        "contactNo",
+        "email",
+        "gender",
+        "marriedStatus",
+        "area",
+        "username",
+        "password",
+        "familyFlag",
+        "position",
+
+      ];
+
+export default function View() {
+  const [customDataset, setCustomDataset] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getClinicDates();
+        const data = response.data.map((row, index) => ({
+          id: index + 1, // Generate unique id for each row
+          ...row,
+        }));
+        setCustomDataset(data);
+      } catch (error) {
+        console.log("Error getting data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <div style={{ display: "flex", height: "100vh", overflowX: "hidden" }}>
+        <DisplaySidebar />
+        <div style={{ flex: 1, overflowX: "hidden" }}>
+          <div style={{ height: "100vh", width: "100%" }}>
+            <Typography
+              variant="h5"
+              style={{
+                marginBottom: "10px",
+                color: "#2A777C",
+                paddingBottom: "60px",
+                paddingLeft: "10px",
+                paddingTop: "40px",
+              }}
+            >
+              Register Users
+            </Typography>
+
+            <DataGrid
+              autoHeight
+              rows={customDataset}
+              columns={VISIBLE_FIELDS.map((field) => ({
+                field,
+                headerName: field,
+                width: 150, // Adjust width as needed
+              }))}
+              components={{ Toolbar: GridToolbar }}
+            />
+          </div>
+        </div>
+      </div>
+    </Box>
+  );
+}
+
+
